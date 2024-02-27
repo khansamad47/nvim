ba7b2de8dcd895c916341ff4e1628351cdacb4db
Samad Khan <khan.samad47@gmail.com>
2024-02-26 20:18:12 -0500

    Reordering

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,11 +3,11 @@
 -- `:options` to display current options settings
 
 -- Tab / Indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.autoindent = true
 vim.opt.expandtab = true
-vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
 -- Search
 vim.opt.hlsearch = true
