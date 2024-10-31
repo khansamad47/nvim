7885a03108e6fa3e578a24e4f6f24503ca51ce1d
Samad Khan <khan.samad47@gmail.com>
2024-10-31 14:37:19 -0400

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,3 +2,9 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", 
 	{ noremap = true, silent = true }
 )
+
+-- For bbg cpp formatting
+vim.api.nvim_set_keymap("v", "<C-I>",
+    ":!/opt/bb/bin/bde-format<CR>",
+    { noremap = true, silent = true }
+)
